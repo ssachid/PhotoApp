303c99682193143c1c058b02b07d8bb6d6483105
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-16 13:21:16 -0800

    Migrated images database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215162730) do
+ActiveRecord::Schema.define(version: 20180216212000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "payments", force: :cascade do |t|
     t.string "email"
@@ -44,4 +53,5 @@ ActiveRecord::Schema.define(version: 20180215162730) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
 end
